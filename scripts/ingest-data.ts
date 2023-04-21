0a180eef611723c14686de1cd75e816adec471cf
Elijah Amian <elijah25@uw.edu>
2023-04-21 12:32:45 -0700

    Incorporate uploading file in different namespace based on the folder it was in.

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -23,50 +23,45 @@ export const run = async () => {
     });
 
     // const loader = new PDFLoader(filePath);
-    const rawDocs = await directoryLoader.load();
-    console.log(rawDocs);
+    let rawDocs = await directoryLoader.load();
+
     /* Split text into chunks */
     const textSplitter = new RecursiveCharacterTextSplitter({
       chunkSize: 1000,
       chunkOverlap: 200,
     });
 
-    const docs = await textSplitter.splitDocuments(rawDocs);
-    const json = JSON.stringify(Array.from(docs.entries()));
-    await fs.writeFile('test.json', json);
+    for (let i = 0; i < rawDocs.length; i++) {
+      const doc = await textSplitter.splitDocuments([rawDocs[i]]);
+      const json = JSON.stringify(Array.from(doc.entries()));
 
-    let directory = path.dirname(docs[2].metadata.source);
-    const splitPath = directory.split(path.sep);
-    const namespace = splitPath[splitPath.length - 1];
+      let directory = path.dirname(rawDocs[i].metadata.source);
+      const splitPath = directory.split(path.sep);
+      const namespace = 'Linear-Algebra' + splitPath[splitPath.length - 1];
 
-    console.log('namespace: ', namespace);
+      await fs.writeFile(`${namespace}.json`, json);
+      // console.log('namespace: ', namespace);
 
-    //const namespace = path.basename(docs[0].metadata.source);
-    console.log('doc count:', docs.length);
+      //const namespace = path.basename(docs[0].metadata.source);
+      console.log('doc count:', doc.length);
 
-    console.log('creating vector store...');
-    /*create and store the embeddings in the vectorStore*/
-    const embeddings = new OpenAIEmbeddings();
-    const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
+      console.log('creating vector store...');
+      /*create and store the embeddings in the vectorStore*/
+      const embeddings = new OpenAIEmbeddings();
+      const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
 
-    //embed the PDF documents
-    // await PineconeStore.fromDocuments(docs, embeddings, {
-    //   pineconeIndex: index,
-    //   namespace: PINECONE_NAME_SPACE,
-    //   textKey: 'text',
-    // });
-
-    // const splitDocs = docs;
-    // const upsertChunkSize = 50;
-    // for (let i = 0; i < splitDocs.length; i += upsertChunkSize) {
-    //   const chunk = splitDocs.slice(i, i + upsertChunkSize);
-    //   console.log('chunk', i, chunk);
-    //   await PineconeStore.fromDocuments(chunk, embeddings, {
-    //     pineconeIndex: index,
-    //     namespace: PINECONE_TEST_NAME_SPACE,
-    //     textKey: 'text',
-    //   });
-    // }
+      const splitDocs = doc;
+      const upsertChunkSize = 50;
+      // for (let i = 0; i < splitDocs.length; i += upsertChunkSize) {
+      //   const chunk = splitDocs.slice(i, i + upsertChunkSize);
+      //   console.log('chunk', i, chunk);
+      //   await PineconeStore.fromDocuments(chunk, embeddings, {
+      //     pineconeIndex: index,
+      //     namespace: PINECONE_TEST_NAME_SPACE,
+      //     textKey: 'text',
+      //   });
+      // }
+    }
   } catch (error) {
     console.log('error', error);
     throw new Error('Failed to ingest your data');
